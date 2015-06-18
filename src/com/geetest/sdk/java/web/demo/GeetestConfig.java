a54cf5185e94fcf69b695458b0524330e2d93c90
zheng <dreamzsm@gmail.com>
2015-06-18 16:12:07 +0800

    3.15.6.18.2

--- src/com/geetest/sdk/java/web/demo/GeetestConfig.java
+++ src/com/geetest/sdk/java/web/demo/GeetestConfig.java
@@ -9,8 +9,8 @@ package com.geetest.sdk.java.web.demo;
 public class GeetestConfig {
 
 	// TODO: replace the these two string with your own captcha's id/key,the id/key below is just for demo
-	private static final String captcha_id = "626ddf82c800a41272d1da5591905ca9";
-	private static final String private_key = "24a612ae8c62203f724c81a5a9b4e761";
+	private static final String captcha_id = "4a0eb5e6f780944308954bc602814a53";
+	private static final String private_key = "03e0d35820a741d74445761c2b6d38c5";
 
 	public static final String getCaptcha_id() {
 		return captcha_id;
